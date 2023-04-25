b83ed4df4103ea08ec5dc1258f3152c77d028ba9
Jianan Sun <isunjn@gmail.com>
2023-04-25 23:49:26 +0800

    chore: unboxed unread number

--- src/routes/feed/ListMetaEntry.svelte
+++ src/routes/feed/ListMetaEntry.svelte
@@ -19,16 +19,13 @@
   <div class="p-1 mr-2"><Icon /></div>
   <span class="grow text-ellipsis overflow-hidden whitespace-nowrap">{name}</span>
   {#if unread !== 0}
-    <span class="ml-1 px-1.5 rounded-full text-xs
-      {active ? "bg-accent-400 text-white" : "bg-ink-100/75 text-ink-700"}"
-    >
+    <span class="px-1.5 text-xs">
       {unread}
     </span>
   {/if}
   <button class="shrink-0 w-0 overflow-hidden {unread !== 0 && "transition-[width] duration-100"}
     group-hover/item:w-5 group-focus-visible/item:w-5 focus-visible:w-5
-    group-hover/item:p-0.5 group-focus-visible/item:p-0.5 focus-visible:p-0.5
-    group-hover/item:ml-1 group-focus-visible/item:ml-1 focus-visible:ml-1"
+    group-hover/item:p-0.5 group-focus-visible/item:p-0.5 focus-visible:p-0.5"
   >
     <IconMore />
   </button>
