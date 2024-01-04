819bab3e504047b8c7bcd333a9490d1394d8457e
Jianan Sun <isunjn@gmail.com>
2024-01-04 23:43:34 +0800

    ui: prototypeing

--- src/lib/components/biz/Read/TopNav.svelte
+++ src/lib/components/biz/Read/TopNav.svelte
@@ -2,34 +2,63 @@
   import { goto } from "$app/navigation";
   import Icon from "$lib/components/unit/Icon.svelte";
   import {
+    AlignLeftIcon,
     ArchiveIcon,
+    ArrowDownIcon,
+    ArrowLeftIcon,
+    ArrowUpIcon,
     BookmarkIcon,
     CaseSensitiveIcon,
+    CheckIcon,
+    ChevronDownIcon,
     ChevronLeftIcon,
+    ChevronUpIcon,
     CoffeeIcon,
+    CompassIcon,
     MoreHorizontalIcon,
     MoreVerticalIcon,
+    PenLineIcon,
+    ViewIcon,
+    Wand2Icon,
   } from "lucide-svelte";
+  import { IconReadList } from "$lib/assets/icons";
 </script>
 
 <div
   class="sticky top-0 z-30 flex w-full items-center justify-between bg-ink-bg px-4 sm:px-4 py-2.5 sm:py-3"
 >
   <!-- back button -->
-  <button
-    class="p-1.5 rounded-full hover:text-ink-primary cursor-pointer"
-    on:click={() => goto("/list/today")}
-  >
-    <Icon which={ChevronLeftIcon} sz="md" />
-  </button>
+  <div class="flex gap-2">
+    <button
+      class="p-2 rounded-full hover:text-ink-primary cursor-pointer"
+      on:click={() => goto("/list/today")}
+    >
+      <Icon which={ArrowLeftIcon} />
+    </button>
+    <section class="flex gap-1">
+      <!-- <button class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={ChevronUpIcon} />
+      </button>
+      <button class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={ChevronDownIcon} />
+      </button> -->
+      <button class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={AlignLeftIcon} />
+      </button>
+    </section>
+  </div>
+
   <!-- mobile right menu -->
   <button class="sm:hidden p-2">
     <Icon which={MoreHorizontalIcon} />
   </button>
 
   <!-- post actions -->
-  <div class="hidden sm:flex items-center gap-12">
+  <div class="hidden sm:flex items-center gap-8">
     <section class="flex gap-1 items-center">
+      <div class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={CheckIcon} />
+      </div>
       <div class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
         <Icon which={CoffeeIcon} />
       </div>
@@ -41,8 +70,17 @@
       </div>
     </section>
     <section class="flex items-center gap-1">
+      <!-- <div class="p-1.5 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={Wand2Icon} />
+      </div> -->
+      <!-- <div class="p-1.5 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={ViewIcon} />
+      </div>
       <div class="p-1.5 rounded-full hover:text-ink-primary cursor-pointer">
-        <Icon which={CaseSensitiveIcon} sz="md" />
+        <Icon which={CompassIcon} />
+      </div> -->
+      <div class="p-1.5 rounded-full hover:text-ink-primary cursor-pointer">
+        <Icon which={PenLineIcon} />
       </div>
       <div class="p-2 rounded-full hover:text-ink-primary cursor-pointer">
         <Icon which={MoreVerticalIcon} />
