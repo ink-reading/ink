8abbe3cf33ebc44685c19523858ed179dd0147c8
Jianan Sun <isunjn@gmail.com>
2024-04-06 18:31:37 +0800

    ui: login page

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -3,39 +3,45 @@
   export let form;
 </script>
 
-<main class="flex h-screen items-center justify-between text-ink-text">
-  <div class="h-full w-2/5 bg-primary"></div>
+<main class="flex h-screen items-center justify-center text-prose">
   <div class="flex grow items-center justify-center">
-    <div class="w-96">
+    <div class="w-96 px-4">
+      <div class="pb-12 flex justify-between items-end gap-4 font-mono leading-none">
+        <div class="text-primary text-md pb-px font-medium">ink</div>
+        <div class="text-prose-pale text-xs">/// UNDER CONSTRUCTION</div>
+      </div>
       <form method="post" use:enhance>
         <div class="pb-4">
-          <label for="username" class="block py-1">Username</label>
+          <!-- <label for="username" class="block py-1">Username</label> -->
           <input
             name="username"
             id="username"
-            placeholder="Enter your username"
-            class="w-full rounded border-2 border-roam px-4 py-2 outline-none ring-primary ring-offset-0 placeholder:text-ink-text/30 focus-visible:border-primary"
+            placeholder="Username"
+            class="w-full rounded bg-roam px-4 py-2 outline-none ring-primary ring-offset-0 placeholder:text-prose-pale focus-visible:border-primary"
           />
         </div>
 
         <div class="">
-          <label for="password" class="block py-1">Password</label>
+          <!-- <label for="password" class="block py-1">Password</label> -->
           <input
             type="password"
             name="password"
             id="password"
-            placeholder="Enter your password"
-            class="w-full rounded border-2 border-roam px-4 py-2 outline-none ring-primary ring-offset-0 placeholder:text-ink-text/30 focus-visible:border-primary"
+            placeholder="Password"
+            class="w-full rounded bg-roam px-4 py-2 outline-none ring-primary ring-offset-0 placeholder:text-prose-pale focus-visible:border-primary"
           />
         </div>
 
-        <div class="mb-6 mt-4 min-h-[24px] text-red-500">
-          {#if form?.message}
+        {#if form?.message}
+          <div class="mt-3 min-h-[24px] text-aux-danger">
             {form.message}
-          {/if}
-        </div>
+          </div>
+        {/if}
 
-        <button class="w-full rounded bg-primary p-2 text-white">Login</button>
+        <button
+          class="mt-6 w-full rounded border-2 border-roam-accent p-2 text-prose-pale hover:text-primary"
+          >Login</button
+        >
       </form>
     </div>
   </div>
